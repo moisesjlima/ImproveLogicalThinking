b718a75e6075d2a28dc6db16536a5dafdf1e1200
moisesjlima <rdsociallima@outlook.com>
2023-05-22 18:28:49 -0300

    adding recursion method in fibonacci

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,14 +6,20 @@
         {
             // Fibonacci sequence
             // the number start with 0 1 1 and the next numbers is the sum of the two previous numbers, exemplo: 1 1 2 3 5 8 13 21... (1 + 1) = 2 (2 + 1) = 3 ....
-            // * We use fibonacci sequence on story points in Azure DevOps to rank the task difficulty
+            // * We use fibonacci sequence on story points in Azure DevOps to rank the task 
+            int terms = 20;
 
-            int terms = 18;
+            Console.WriteLine($"fibonacci in position {terms}: " + FibonacciSequenceRecursion(20)); //Using recursion
+
+            Console.WriteLine();
+
             Console.WriteLine("Fibonacci Sequence:");
-            foreach (int number in FibonacciSequence(terms))
+            foreach (int number in FibonacciSequence(terms + 1))
             {
                 Console.Write(number + " ");
             }
+
+            Console.WriteLine();
         }
 
         private static int[] FibonacciSequence(int terms)
@@ -64,6 +70,14 @@
             {
                 yield return fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2]; //yield return cannot be in try catch block
             }
+        }
+
+        private static int FibonacciSequenceRecursion(int number)
+        {
+            if (number == 0 || number == 1)
+                return number;
+
+            return FibonacciSequenceRecursion(number - 1) + FibonacciSequenceRecursion(number - 2);
         }
 
     }
