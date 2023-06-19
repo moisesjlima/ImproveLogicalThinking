fb961d3898e42240dbfc0127038f73173d8d109e
moisesjlima <rdsociallima@outlook.com>
2023-06-19 13:58:53 -0300

    adding new string format in c# version 11

---
+++ JsonString/JsonString.cs
@@ -0,0 +1,14 @@
+﻿namespace JsonString
+{
+    internal class JsonString
+    {
+        static void Main(string[] args)
+        {
+            var stringWay = "{nome: \"Moisés\" , idade: \"23\"}";
+
+            ///var stringWay = """{nome: "Moisés" , idade: "23"}"""; // c# version available >= 11
+
+            Console.WriteLine(stringWay);
+        }
+    }
+}
